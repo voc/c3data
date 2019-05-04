b071fa9a9ff12f2401cefea307ef404d510b5d6d
Andreas Hubel <andi@saerdnaer.de>
2019-05-04 18:56:56 +0200

    add base for graphql gateway

---
+++ c3d-graphql-gateway/src/index.ts
@@ -0,0 +1,75 @@
+import { createServer } from 'http';
+
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { ApolloServer, SchemaDirectiveVisitor } from 'apollo-server-express';
+import { execute, subscribe, GraphQLSchema } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+
+//import persistentQueries from './util/persistentQueries';
+import { graphiqlExpress } from './util/graphiql';
+import mergeSchemas from './schema';
+//import schemaDirectives from './schema/directives';
+const schemaDirectives = null;
+
+
+const isDevelopment = true;
+const app = express();
+
+async function run() {
+	app.use('*', cors({ origin: '*' }));
+
+	const cacheMiddleware = cache.createMiddleware({
+		isEnabled: false,
+		defaultCacheTTL: 10 * 1000,
+		shouldAttachMembership: (req, res) => !!req.header('x-add-membership'),
+	});
+
+	app.use(cacheMiddleware);
+
+	const graphqlPath = '/graphql';
+	const schema: GraphQLSchema = await mergeSchemas();
+
+	// setup directives
+	SchemaDirectiveVisitor.visitSchemaDirectives(schema, schemaDirectives);
+
+  const port = process.env.port || 5000;
+	const playgroundOptions: any = isDevelopment
+			? {
+					endpoint: graphqlPath,
+					subscriptionEndpoint: `ws://localhost:${port}/subscriptions`,
+					settings: {
+						// Force setting, workaround: https://github.com/prisma/graphql-playground/issues/790
+						'editor.theme': 'dark',
+						'editor.cursorShape': 'line', // possible values: 'line', 'block', 'underline'
+					},
+			  }
+			: false;
+
+	// see https://www.apollographql.com/docs/apollo-server/api/apollo-server.html
+	const apolloServer = new ApolloServer({
+		schema,
+		context: ({ req, res }: { req: Request; res: Response }) => ({ req, res }),
+		debug: isDevelopment,
+		introspection: isDevelopment,
+		playground: playgroundOptions,
+	});
+
+	app.use(graphqlPath, bodyParser.json()); //, persistentQueries);
+
+	// still provide old GraphiQL interface
+	if (isDevelopment) {
+		app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+	}
+
+	apolloServer.applyMiddleware({ app, path: graphqlPath });
+
+	const server = createServer(app);
+	server.listen(port, () => {
+		console.log(`graphql gateway running on port ${port}`);
+
+		new SubscriptionServer({ execute, subscribe, schema }, { server, path: '/subscriptions' });
+	});
+}
+run();
---
+++ c3d-graphql-gateway/src/util/graphiql.ts
@@ -0,0 +1,48 @@
+import * as express from 'express';
+import * as url from 'url';
+import {
+  GraphQLOptions,
+  HttpQueryError,
+  runHttpQuery,
+} from 'apollo-server-core';
+import { print, parse } from 'graphql';
+import * as GraphiQL from 'apollo-server-module-graphiql';
+
+
+export interface ExpressGraphiQLOptionsFunction {
+	(req?: express.Request): GraphiQL.GraphiQLData | Promise<GraphiQL.GraphiQLData>;
+}
+
+/* This middleware returns the html for the GraphiQL interactive query UI
+ *
+ * GraphiQLData arguments
+ *
+ * - endpointURL: the relative or absolute URL for the endpoint which GraphiQL will make queries to
+ * - (optional) query: the GraphQL query to pre-fill in the GraphiQL UI
+ * - (optional) variables: a JS object of variables to pre-fill in the GraphiQL UI
+ * - (optional) operationName: the operationName to pre-fill in the GraphiQL UI
+ * - (optional) result: the result of the query to pre-fill in the GraphiQL UI
+ */
+
+export function graphiqlExpress(options: GraphiQL.GraphiQLData | ExpressGraphiQLOptionsFunction) {
+	const graphiqlHandler = (req: express.Request, res: express.Response, next: Function) => {
+		let query: any = req.url && url.parse(req.url, true).query;
+		if ('query' in query) {
+			// prettify query from url
+			try {
+				query.query = print(parse(query.query));
+			}
+			catch {}
+		}
+		GraphiQL.resolveGraphiQLString(query, options, req).then(
+			(graphiqlString: any) => {
+				res.setHeader('Content-Type', 'text/html');
+				res.write(graphiqlString);
+				res.end();
+			},
+			(error) => next(error),
+		);
+	};
+
+	return graphiqlHandler;
+}
